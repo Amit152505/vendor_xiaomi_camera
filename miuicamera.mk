6dd72e1e33144c132ed4e1b91c2ad213b963deab
AmitBN <amit.b.n..2002@gmail.com>
2023-07-30 16:12:55 +0000

    MIUI camera updated to latest version

--- miuicamera.mk
+++ miuicamera.mk
@@ -14,7 +14,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.vendor.camera.privapp.list=org.codeaurora.snapcam,com.android.camera \
     ro.com.google.lens.oem_camera_package=com.android.camera \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.aperture,org.lineageos.aperture.dev \
+    ro.hardware.camera=xiaomi
 
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += \
